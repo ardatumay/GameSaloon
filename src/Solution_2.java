4314c978921f71ee3d75db53fa7c5c9123da9625
Hayri Durmaz <hayri.durmaz@tedu.edu.tr>
2018-03-24 23:41:42 +0300

    classes added

--- src/Solution_2.java
+++ src/Solution_2.java
@@ -1,28 +1,41 @@
+import java.util.Arrays;
+import java.util.Comparator;
 import java.util.Scanner;
 
 public class Solution_2 {
-	
+	static class Prism{
+		int x,y,z,hacim;
+		Prism(int a, int b, int c){
+			x=a;y=b;z=c;
+			hacim=x*y*z;
+		}
+		
+	}
+	static class Hacmegoresort implements Comparator<Prism>
+	{
+	    public int compare(Prism a, Prism b)
+	    {
+	        return a.hacim - b.hacim;
+	    }
+	}
 	
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
 		int N, x,y,z;
 		
 		N=scan.nextInt();
-		x=scan.nextInt();
-		y=scan.nextInt();
-		z=scan.nextInt();
+		Prism ODA = new Prism(scan.nextInt(), scan.nextInt(), scan.nextInt());
 		
+		Prism [] Kutular = new Prism[N];
 		int [][] Kupler = new int [N][3];
 		
 		for (int i = 0; i < N; i++) {
-			for (int j = 0; j < Kupler[i].length; j++) {
-				Kupler[i][j]=scan.nextInt();
-			}
+			Kutular[i]=new Prism(scan.nextInt(), scan.nextInt(), scan.nextInt());
 		}
-		int Hacimler[] = new int[N];
-		for (int i = 0; i < Hacimler.length; i++) {
-			Hacimler[i]=
-		}
+		
+		Arrays.sort(Kutular, new Hacmegoresort());
+		
+		
 	}
 
 }
