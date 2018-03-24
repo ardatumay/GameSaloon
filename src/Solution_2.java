f1199e9bf327fd9401e6e41051cbe0af1e560d93
Hayri Durmaz <hayri.durmaz@tedu.edu.tr>
2018-03-24 23:18:20 +0300

    sol1 and sol2

---
+++ src/Solution_2.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Solution_2 {
+	
+	
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		int N, x,y,z;
+		
+		N=scan.nextInt();
+		x=scan.nextInt();
+		y=scan.nextInt();
+		z=scan.nextInt();
+		
+		int [][] Kupler = new int [N][3];
+		
+		for (int i = 0; i < N; i++) {
+			for (int j = 0; j < Kupler[i].length; j++) {
+				Kupler[i][j]=scan.nextInt();
+			}
+		}
+		int Hacimler[] = new int[N];
+		for (int i = 0; i < Hacimler.length; i++) {
+			Hacimler[i]=
+		}
+	}
+
+}
