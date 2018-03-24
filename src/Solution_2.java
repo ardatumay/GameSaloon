cb1af55d0a43922b81b6bf82828b39303ea8cf8f
Hayri Durmaz <hayri.durmaz@tedu.edu.tr>
2018-03-25 00:07:12 +0300

    odayaSigarMi method written

--- src/Solution_2.java
+++ src/Solution_2.java
@@ -5,6 +5,7 @@ import java.util.Scanner;
 public class Solution_2 {
 	static class Prism{
 		int x,y,z,hacim;
+		boolean [] isFitOda={false,false,false,false,false,false};
 		Prism(int a, int b, int c){
 			x=a;y=b;z=c;
 			hacim=x*y*z;
@@ -36,6 +37,32 @@ public class Solution_2 {
 		Arrays.sort(Kutular, new Hacmegoresort());
 		
 		
+	}
+	public static void odayaSigarMi(Prism p1,Prism p2){
+		if(p1.x<=p2.x){
+			if(p1.y<=p2.y && p1.z<=p2.z){
+				p1.isFitOda[0]=true;
+			}
+			if(p1.z<=p2.y && p1.y<=p2.z){
+				p1.isFitOda[1]=true;
+			}
+		}
+		if(p1.x<=p2.y){
+			if(p1.y<=p2.x && p1.z<=p2.z){
+				p1.isFitOda[2]=true;
+			}
+			if(p1.y<p2.z && p1.z<=p2.x){
+				p1.isFitOda[3]=true;
+			}
+		}
+		if(p1.x<=p2.z){
+			if(p1.y<=p2.x && p1.z<p2.y){
+				p1.isFitOda[4]=true;
+			}
+			if(p1.y<p2.y && p1.z<p2.x){
+				p1.isFitOda[5]=true;
+			}
+		}
 	}
 
 }
