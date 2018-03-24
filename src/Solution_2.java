619ee157b31b3ea7ce50bd8b5cce2d00750f0245
Arda Tumay <ardatm15@gmail.com>
2018-03-25 00:15:14 +0300

    Filter kutular methodu eklendi.(solution 2)

--- src/Solution_2.java
+++ src/Solution_2.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Comparator;
 import java.util.Scanner;
@@ -18,7 +19,18 @@ public class Solution_2 {
 	        return a.hacim - b.hacim;
 	    }
 	}
-	
+	private static Prism[] filterInput(Prism[] kutular, Prism oda){
+		ArrayList<Prism> filteredKutular = new ArrayList<Prism>();
+		for(int i = 0; i < kutular.length; i++){
+			if(((kutular[i].x > oda.x) && (kutular[i].x > oda.y) && (kutular[i].x > oda.z) && (kutular[i].y > oda.x) && (kutular[i].y > oda.y) && (kutular[i].y > oda.z) && (kutular[i].z > oda.z) && (kutular[i].z > oda.x) && (kutular[i].z > oda.y)) || (kutular[i].hacim > oda.hacim)){
+
+			}else{
+				filteredKutular.add(kutular[i]);
+			}
+		}
+		Prism [] resultKutular = (Prism[]) filteredKutular.toArray();
+		return resultKutular;
+	}
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
 		int N, x,y,z;
@@ -32,7 +44,9 @@ public class Solution_2 {
 		for (int i = 0; i < N; i++) {
 			Kutular[i]=new Prism(scan.nextInt(), scan.nextInt(), scan.nextInt());
 		}
-		
+
+
+		Kutular = filterInput(Kutular, ODA);
 		Arrays.sort(Kutular, new Hacmegoresort());
 		
 		
